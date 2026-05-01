7fb1f623722af2f23590a12c80d0a986d2c4b1a8
GajiPH13 <islamgajinajrul@gmail.com>
2026-05-01 17:50:13 +0200

    Project initieted. Nav Bar, Banner and Footer has added. JSON Data is provided in Public file

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
+const Banner = () => {
+  return (
+    <div className="bg-[url('/Banner.png')] mt-5 h-[60vh]
+     max-w-7xl mx-auto w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl">
+      {/* Overlay */}
+      
+    </div>
+  );
+};
+
+export default Banner;
