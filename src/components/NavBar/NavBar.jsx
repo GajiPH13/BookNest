a28130beeba49086c489f6439a9fe9d7b47ff6c8
GajiPH13 <islamgajinajrul@gmail.com>
2026-05-02 14:11:12 +0200

    Error on gitPush

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,64 +12,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   // console.log(userData);
   return (
-    // <div className="border-b px-2">
-    //   <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
-    //     <Link href="/">
-    //     <div className="flex gap-2 items-center">
-    //       <Image
-    //         src={"/NavBarLogo.png"}
-    //         alt="logo"
-    //         loading="eager"
-    //         width={30}
-    //         height={30}
-    //         className="object-cover h-auto w-auto"
-    //       />
-    //       <h3 className="font-black text-lg">BookNest</h3>
-    //     </div>
-    //     </Link>
-
-    //     <ul className="flex items-center gap-5 text-sm">
-    //       <li>
-    //         <Link href={"/"}>Home</Link>
-    //       </li>
-    //       <li>
-    //         <Link href={"/all-photos"}>All Photos</Link>
-    //       </li>
-          
-    //       <li>
-    //         <Link href={"/profile"}>Profile</Link>
-    //       </li>
-    //     </ul>
-
-    //     <div className="flex gap-4">
-    //       {!user && (
-    //         <ul className="flex items-center gap-4  text-sm">
-    //           <li>
-    //             <Link href={"/signup"}>SignUp</Link>
-    //           </li>
-    //           <li>
-    //             <Link href={"/signin"}>SignIn</Link>
-    //           </li>
-    //         </ul>
-    //       )}
-    //       {user && (
-    //         <div className="flex items-center gap-2">
-    //           <Avatar>
-    //             <Avatar.Image
-    //               alt={user.name}
-    //               src={user.image}
-    //               // for google login to show the avatar
-    //               referrerPolicy="no-referrer"
-    //             />
-    //             <Avatar.Fallback>{user.name[0]}</Avatar.Fallback>
-    //           </Avatar>
-    //           <p>{user.name}</p>
-    //           <Button variant="outline" size="sm" className={'bg-red-500 text-white'} onClick={() => authClient.signOut()}>SignOut</Button>
-    //         </div>
-    //       )}
-    //     </div>
-    //   </nav>
-    // </div>
+    
      <div className="border-b px-2 relative">
       <nav className="flex justify-between items-center py-3 max-w-7xl mx-auto w-full">
 
@@ -150,7 +93,7 @@ const Navbar = () => {
 
         <div className="flex flex-col gap-4 p-4 text-sm">
           <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
-          <Link href="/all-photos" onClick={() => setIsOpen(false)}>All Photos</Link>
+          <Link href="/all-books" onClick={() => setIsOpen(false)}>All Photos</Link>
           <Link href="/profile" onClick={() => setIsOpen(false)}>Profile</Link>
 
           {!user && (
