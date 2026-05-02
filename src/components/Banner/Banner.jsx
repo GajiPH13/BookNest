811030c184c21eadddbdbbedc495dee78feb1366
GajiPH13 <islamgajinajrul@gmail.com>
2026-05-02 14:27:29 +0200

    can not find the Bug

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,33 +3,7 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    // <div className="bg-[url('/Banner.png')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl mt-6">
-    //   {/* Overlay */}
-    //   <div className="w-full h-full rounded-lg  flex items-center ">
-    //     <div className="  px-30 space-y-4 ">
-    //       <p className="text-5xl text-black  font-bold  ">
-    //         Find Your Next Read
-    //       </p>
-    //       <p className="text-5xl text-[#386fec]  font-bold  ">
-    //         Your One-Stop Destination
-    //       </p>
-
-    //       <p className="text-lg md:text-md mb-6 max-w-xl text-[#4a5b7d]">
-    //         Explore thousands of books, eBooks, and resources
-    //         <br />
-    //         anytime anywhere.
-    //       </p>
-
-    //       <div className="flex gap-4">
-    //         <Link href="/all-books">
-    //           <Button className=" bg-blue-500 text-white hover:bg-[#83a6fd]">
-    //             Browse Now
-    //           </Button>
-    //         </Link>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    
     <div
       className="
     bg-[url('/Banner.png')] 
@@ -56,7 +30,7 @@ const Banner = () => {
 
           <p className="text-sm sm:text-base md:text-lg mb-6 max-w-xl text-[#4a5b7d]">
             Explore thousands of books, eBooks, and resources
-            <br />
+            
             anytime anywhere.
           </p>
 
